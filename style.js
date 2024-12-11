bb44b800a4915b7ff35262a83bb36f9705960ef5
hellsing984 <kevin.sicre.dwwm48@gmail.com>
2024-12-11 12:16:49 +0100

    dark3

--- style.js
+++ style.js
@@ -1,3 +1,13 @@
 function toggleDarkMode() {
+  // Ajoute ou enlève la classe "dark-mode" sur le body pour activer/désactiver le mode sombre
   document.body.classList.toggle("dark-mode");
-} 
+
+  // Change le texte du bouton en fonction de l'état du mode sombre
+  const button = document.querySelector('.darkmode button');
+  if (document.body.classList.contains("dark-mode")) {
+      button.textContent = "Light Mode";
+  } else {
+      button.textContent = "Dark Mode";
+  }
+}
+
