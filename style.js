85a78a8627f942336a7af099d7aae6b2b3692035
hellsing984 <kevin.sicre.dwwm48@gmail.com>
2024-12-11 11:56:33 +0100

    darkmode

--- style.js
+++ style.js
@@ -1,23 +1,3 @@
-var buttonstate=0;
-function onoff(element)
-{
-  buttonstate= 1 - buttonstate;
-  var blabel, bstyle, bcolor;
-  if(buttonstate)
-  {
-    blabel="on";
-    bstyle="green";
-    bcolor="lightgreen";
-  }
-  else
-  {
-    blabel="off";
-    bstyle="lightgray";
-    bcolor="gray";
-  }
-  var child=element.firstChild;
-  child.style.background=bstyle;
-  child.style.color=bcolor;
-  child.innerHTML=blabel;
-}
-
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+} 
